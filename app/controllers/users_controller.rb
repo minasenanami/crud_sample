86ee5c4f2d4226d35f56383b4ea153c3dfbb1d2d
僚 <>
2021-05-06 13:03:57 +0900

    resouces を使用した形式に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
 
   def create
     user = User.create!(user_params)
-    redirect_to action: :index
   end
 
   def edit
@@ -21,15 +20,11 @@ class UsersController < ApplicationController
   def update
     user = User.find(params[:id])
     user.update(user_params)
-
-    redirect_to action: :index
   end
 
   def destroy
     user = User.find(params[:id])
     user.destroy
-
-    redirect_to action: :index
   end
 
 
